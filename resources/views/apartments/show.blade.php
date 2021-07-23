6cbdd1f9315bd312d64a9f9c821a4bacf415ed55
rakhan177 <metboolean@gmail.com>
2021-07-23 10:03:23 +0200

    aggiunta navbar auth

--- resources/views/apartments/show.blade.php
+++ resources/views/apartments/show.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Single Apartment</title>
-</head>
-<body>
-    <h1>{{ $apartment->title }}</h1>
+@extends('layouts/app')
+{{-- @section('pageTitle', 'Home Page') --}}
+@section('content')
+   <h1>{{ $apartment->title }}</h1>
     <img src="{{ $apartment->img_cover }}" alt="casa">
     <br>
     <a href="{{ route('home.index') }}">Torna alla Homepage</a>
-</body>
-</html>
+@endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HOMEPAGE</title>
-</head>
-<body>
+@extends('layouts/app')
+{{-- @section('pageTitle', 'Home Page') --}}
+@section('content')
     <h1>Ciao dalla home</h1>
 
     @foreach($apartments as $apartment)
@@ -17,6 +11,4 @@
             <a href="{{ route('apartments.show', $apartment->id) }}">Vai ai dettagli...</a>
         </div>
     @endforeach
-
-</body>
-</html>
+@endsection
