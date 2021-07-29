da55b8cd5b868e98a1dafa4d3c0300a8084306f7
AgneseBettio <bettio.agnese@gmail.com>
2021-07-29 20:22:10 +0200

    aggiustata view e rotta ora pronta a pagamento

--- app/Http/Controllers/Admin/SponsorshipController.php
+++ app/Http/Controllers/Admin/SponsorshipController.php
@@ -11,6 +11,6 @@ class SponsorshipController extends Controller
 {
     public function index(Apartment $apartment){
         $sponsorships = Sponsorship::all();
-        return view("admin.sponsorships.index", ['apartment' => $apartment]);
+        return view("admin.sponsorships.index", ['apartment' => $apartment, 'sponsorships'=>$sponsorships]);
     }
 }
--- resources/views/admin/sponsorships/index.blade.php
+++ resources/views/admin/sponsorships/index.blade.php
@@ -2,6 +2,24 @@
 @section('pageTitle', 'Dettagli Appartamento')
 @section('content')
 
-prova per trovare rotta
+<div class="container">
 
+<h3>{{ Auth::user()->name }}</h3>
+<p>metti in prima pagina il tuo appartamento {{ $apartment->title }}</p>
+<p>scegli una sponsorizzazione:</p>
+{{-- sponsorizzazione 1 --}}
+@foreach($sponsorships as $sponsorship)
+
+<div class="row">
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">{{$sponsorship->name}}</h5>
+        <p class="card-text">{{$sponsorship->duration}}</p>
+        <p class="card-text">{{$sponsorship->price}}</p>
+      </div>
+    </div>
+  </div>
+</div>
+@endforeach
 @endsection
