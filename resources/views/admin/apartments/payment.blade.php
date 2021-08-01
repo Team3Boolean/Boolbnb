66d68ac83b6fd738fc2092b239c089bf940a867b
Cordovani <lor.cordovani@gmail.com>
2021-08-01 12:49:17 +0200

    paymente view

---
+++ resources/views/admin/apartments/payment.blade.php
@@ -0,0 +1,16 @@
+{{-- la view per i pagamenti --}}
+
+{{-- 
+    Step 1  
+    Your front-end requests a client token from your server and initializes the client SDK. 
+--}}
+
+{{-- 
+    Step 3
+    The customer submits payment information, the client SDK communicates that information to Braintree and returns a payment method nonce. 
+--}}
+
+{{-- 
+    Step 4
+    Your front-end sends the payment method nonce to your server.
+ --}}
