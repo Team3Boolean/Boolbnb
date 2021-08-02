4c7d6249894b030e64779281b829cbbd7c998b4b
AgneseBettio <bettio.agnese@gmail.com>
2021-08-02 18:53:25 +0200

    modifica head in layoutAdmin

--- resources/views/admin/payments/index.blade.php
+++ resources/views/admin/payments/index.blade.php
@@ -2,17 +2,6 @@
 
 @extends('layouts.layoutAdmin')
 @section('pageTitle', 'Aggiungi Appartamento')
-<head>
-    <meta charset="utf-8">
-    <script src="https://js.braintreegateway.com/web/dropin/1.31.0/js/dropin.min.js"></script>
-
-    <!-- includes the Braintree JS client SDK -->
-    <script src="https://js.braintreegateway.com/web/dropin/1.31.0/js/dropin.min.js"></script>
-
-    {{-- <!-- includes jQuery -->
-    <script src="http://code.jquery.com/jquery-3.2.1.min.js" crossorigin="anonymous"></script> --}}
-
-</head>
 @section('content')
 <div class="container">
 		<h3>Ciao <span>{{ Auth::user()->name }}</span>, rendi il tuo annuncio più accattivante e fai in modo che lo vedano più persone!</h3>
@@ -74,7 +63,7 @@ id=sponsorship+SponsorchipName // --}}
             <p id="amount_preview">€ 5.99 </p>
         </label>
         {{-- Drop-in nella pagina (dalla documentazione) --}}
-        <div id="bt-dropin"></div> 
+        <div id="dropin-container"></div> 
 
         <input id="client_token" name="client_token" type="hidden" value="{{ $client_token }}"/>
         <input id="nonce" name="payment_method_nonce" type="hidden" />
