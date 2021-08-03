a000858c99ac5a889b4cf2108be6524f5b37453d
rakhan177 <metboolean@gmail.com>
2021-08-03 18:17:18 +0200

    sistemato merge css

--- public/js/app.js
+++ public/js/app.js
@@ -51018,8 +51018,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Huawei\Desktop\boolean\Boolbnb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Huawei\Desktop\boolean\Boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\rakha\Desktop\Boolbnb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\rakha\Desktop\Boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
